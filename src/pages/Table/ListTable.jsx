24b65d3502fed2d540263695b7456944065284b2
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-08-29 14:03:49 -0300

    Feat: Criação da tela de listagem em cards

--- src/pages/Table/ListTable.jsx
+++ src/pages/Table/ListTable.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import "./ListTable.css";
+
 export function Table() {
-    return(
-        <>
-        <h1>Lista de instrumentos em Tabela</h1>
-        </>
-    )
+  const [instrumentos, setInstrumentos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  useEffect(() => {
+    async function fetchInstrumentos() {
+      try {
+        const resposta = await fetch("http://localhost:3000/equipamentos");
+        if (!resposta.ok) {
+          throw new Error("Erro ao buscar instrumentos");
+        }
+        const dados = await resposta.json();
+        setInstrumentos(dados);
+      } catch (err) {
+        setErro(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchInstrumentos();
+  }, []);
+
+  if (loading) return <p>Carregando instrumentos...</p>;
+  if (erro) return <p>Erro: {erro}</p>;
+
+  return (
+ <div className="container-cards">
+      {instrumentos.map((item) => (
+        <div key={item.id} className="card">
+          <h3>{item.nome}</h3>
+          <p><strong>Categoria:</strong> {item.categoria}</p>
+          <p><strong>Marca:</strong> {item.marca}</p>
+          <p><strong>Preço:</strong> R$ {item.preco}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
